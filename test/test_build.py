e7f26d223979e1279074e22c6f5912e6a66d17df
yan.yan <yanyan.sub@outlook.com>
2022-09-23 21:54:02 +0800

    fix pytest

--- test/test_build.py
+++ test/test_build.py
@@ -7,13 +7,6 @@ from ccimport.utils import tempdir
 import os 
 import sys 
 
-def test_cpp_build():
-    source = ccimport.autoimport([Path(__file__).parent / "source.cc"],
-                                 Path(__file__).parent / "source")
-    assert source.sub(2, 1) == 1
-    obj = source.TestClass(5)
-    assert obj.add(3) == 8
-
 
 def test_cpp_exec_build():
     with tempdir() as tempd:
